ad0e4989c6bc0e77bfeb7146f0ed764324a3fb25
Mahmud Allahverdiyev <tr0j4n034@gmail.com>
2018-10-28 17:50:10 +0300

    Initial Commit

---
+++ C++ Big Integer Library/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Big Integer Library
+//
+//  Created by Mahmud on 10/28/18.
+//  Copyright © 2018 Mahmud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
