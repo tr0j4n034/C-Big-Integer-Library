b4c5941e8f4f36dcda70743ec393d0401cd8786b
Mahmud Allahverdiyev <tr0j4n034@gmail.com>
2018-11-14 10:07:30 +0300

    2x faster

--- C++ Big Integer Library/prime_factory.h
+++ C++ Big Integer Library/prime_factory.h
@@ -28,17 +28,11 @@ int _log(int length) {
     }
     return l;
 }
-bool FermatWitness(int base, Integer I) { // base-2 strong primality test
-    Integer N = I - 1;
-    int s = 0;
-    while (N.isEven()) {
-        N >>= 1;
-        s += 1;
-    }
-    Integer d = N;
+bool FermatWitness(int base, int s, Integer d, Integer I) { // base-2 strong primality test
     if (modPowerI(base, d, I) == 1) {
         return false;
     }
+    cout << "s = " << s << ", d = " << d << endl;
     Integer current = powFastI(base, d);
     for (int i = 0; i < s; i ++) {
         if (current % I == (I - 1)) {
@@ -52,6 +46,7 @@ bool isPrime(Integer I) {
     if (I < 2) {
         return false;
     }
+    clock_t t0 = clock();
     for (int i = 0; i < baseSize; i ++) {
         if (I == basePrimes[i]) {
             return true;
@@ -59,25 +54,40 @@ bool isPrime(Integer I) {
             return false;
         }
     }
+    clock_t t1 = clock();
+    Integer N = I - 1;
+    int s = 0;
+    while (N.isEven()) {
+        N >>= 1;
+        s += 1;
+    }
+    Integer d = N;
+    cout << "elapsed_1: " << double(t1 - t0) / CLOCKS_PER_SEC << endl;
     for (int i = 0; i < baseSize; i ++) {
-        if (FermatWitness(basePrimes[i], I)) {
+        if (FermatWitness(basePrimes[i], s, d, I)) {
             return false;
         }
     }
+    clock_t t2 = clock();
+    cout << "elapsed_2: " << double(t2 - t1) / CLOCKS_PER_SEC << endl;
+    
     Random rng(0xabc);
     for (int i = 0; i < EXTRA_FERMAT_TRIALS; i ++) {
         int base = rng.generate(19, 0xfff);
-        if (FermatWitness(base, I)) {
+        if (FermatWitness(base, s, d, I)) {
             return false;
         }
     }
+    clock_t t3 = clock();
+    cout << "elapsed_3: " << double(t3 - t2) / CLOCKS_PER_SEC << endl;
     return true;
 }
 Integer generatePrime(Integer low_bound = (1 << 10)) { // to slow
     Random rng(0xabc);
     Integer result = -1;
     while (result == -1) {
-        result = rng.generateI(low_bound, low_bound << 3);
+        result = rng.generateLikelyPrimeI(low_bound, low_bound << 3);
+        cout << "to check: " << result << endl;
         if (!isPrime(result)) {
             result = -1;
         }
