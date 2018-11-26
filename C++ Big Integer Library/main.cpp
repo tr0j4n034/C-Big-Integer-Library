434246f6486bda3f3b8cfec22340a7945a0a3a12
Mahmud Allahverdiyev <tr0j4n034@gmail.com>
2018-11-26 19:53:17 +0300

    modular inverse testing

--- C++ Big Integer Library/main.cpp
+++ C++ Big Integer Library/main.cpp
@@ -16,21 +16,15 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    Random r(5);
-    cout << r.generate(1, 10) << endl;
-    Integer lowBound = powFastI(2, 100);
+    Integer a = 3;
+    Integer b = 17;
     
-    cout << generateFermatPrime(lowBound) << endl;
-    cout << 1. * clock() / CLOCKS_PER_SEC << endl;
-    cout << generateMillerRabinPrime(lowBound) << endl;
-    //cout << generateEulerPrime(lowBound) << endl;
-    cout << 1. * clock() / CLOCKS_PER_SEC << endl;
-    
-    
-    Integer t = Integer("563959233538003537991201091930755280001354376741888300005859867474093013703");
-    cout << isEulerPrime(t) << endl;
-    cout << isFermatPrime(t) << endl;
-    cout << isMillerRabinPrime(t) << endl;
+//    cout << b / a << endl;
+//    Integer c = Integer("0") - b / a;
+//    cout << c << endl;
+    cout << modularInverse(a, b) << endl;
+    Integer cc = modularInverse(a, b);
+    cout << cc << endl;
     
     return 0;
 }
